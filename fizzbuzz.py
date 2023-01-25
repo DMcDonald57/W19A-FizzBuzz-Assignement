af9c2bca2fe217a5100278cfe12629088cc8bd4c
DMcDonald57 <d.mcdonald579@gmail.com>
2023-01-24 20:40:09 -0700

    quick rough draft of function created.

---
+++ fizzbuzz.py
@@ -0,0 +1,12 @@
+def fizz_buzz(arr):
+    for num in arr:
+        if (num /3):
+            print("Fizz")
+        if (num /5):
+            print("Buzz")
+        # if (num /3 and /5):
+        #     print("FizzBuzz")
+
+num = [1, 30, 7, 55, 57, 23, 9, 17, 235, 1390, 44, 79, 99, 348, 500]
+
+print (fizz_buzz)
