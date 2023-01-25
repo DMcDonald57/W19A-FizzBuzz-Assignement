6831b8ce58c81643aedb2582e33831c07cbacc7b
DMcDonald57 <d.mcdonald579@gmail.com>
2023-01-24 21:22:32 -0700

    first function using divided by 3 and 5 working. testing rest

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,12 @@
 def fizz_buzz(arr):
     for num in arr:
-        if (num /3):
-            print("Fizz")
-        if (num /5):
-            print("Buzz")
-        # if (num /3 and /5):
-        #     print("FizzBuzz")
+        if (num % 3 == 0 and num % 5 == 0):
+            print(num, "FizzBuzz")
+        # elif (num % 3 == 0):
+        #     print("Fizz")
+        #     continue
+        # elif (num % 5 == 0):
+        #     print("Buzz")
 
-num = [1, 30, 7, 55, 57, 23, 9, 17, 235, 1390, 44, 79, 99, 348, 500]
-
-print (fizz_buzz)
+num = [1,30,15,7,55,57,23,9,17,235,1390,44,79,99,348]
+print (fizz_buzz(num))
