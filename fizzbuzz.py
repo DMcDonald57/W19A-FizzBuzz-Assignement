01a06b1dd2c8aa327aeeba54be115c5efb574bbf
DMcDonald57 <d.mcdonald579@gmail.com>
2023-01-25 18:24:32 -0700

    all code working as intended

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,11 +2,10 @@ def fizz_buzz(arr):
     for num in arr:
         if (num % 3 == 0 and num % 5 == 0):
             print(num, "FizzBuzz")
-        # elif (num % 3 == 0):
-        #     print("Fizz")
-        #     continue
-        # elif (num % 5 == 0):
-        #     print("Buzz")
+        elif (num % 3 == 0):
+            print(num, "Fizz")
+        elif (num % 5 == 0):
+            print(num, "Buzz")
 
-num = [1,30,15,7,55,57,23,9,17,235,1390,44,79,99,348]
+num = [1,30,15,7,55,57,23,9,17,235,1390,75,44,79,99,348]
 print (fizz_buzz(num))
